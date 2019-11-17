e73e81c5cc4c56a984fdb5b733d3ed52b780f30e
Myat Mo <myatmo@outlook.com>
2019-11-16 21:43:24 -0600

    Update DisambiguatePose.m

--- DisambiguatePose.m
+++ DisambiguatePose.m
@@ -1,36 +1,26 @@
 function [R,C,X] = DisambiguatePose(R1,C1,X1,R2,C2,X2,R3,C3,X3,R4,C4,X4)
-%Input: R1, C1, X1 · · · R4, C4, X4 are four sets of camera rotation, center, and 3D reconstructed points.
+%Input: R1, C1, X1 Â· Â· Â· R4, C4, X4 are four sets of camera rotation, center, and 3D reconstructed points.
 %Output: R, C, X are the best camera rotation, center, and 3D reconstructed points.
 %Description: The 3D point must lie in front of the both cameras, which can
 %be tested by: r3'(X-C)>0, where r3 is the 3rd row of the rotation matrix.
 
 n = length(X1);
-nValid = [];
-c1 = 0; c2 = 0; c3 = 0; c4 = 0;
+nValid = [0, 0, 0, 0];
+Ri = {R1, R2, R3, R4};
+Ci = {C1, C2, C3, C4};
+Xi = {X1, X2, X3, X4};
 
-for i = 1:n
-    cheirality_X1 = R1(3,:) * (X1(i,:)'-C1);
-    if cheirality_X1 > 0
-        c1 = c1 + 1;
-    end
-    
-    cheirality_X2 = R2(3,:) * (X2(i,:)'-C2);
-    if cheirality_X2 > 0
-        c2 = c2 + 1;
-    end
-    
-    cheirality_X3 = R3(3,:) * (X3(i,:)'-C3);
-    if cheirality_X3 > 0
-        c3 = c3 + 1;
-    end
-    
-    cheirality_X4 = R4(3,:) * (X4(i,:)'-C4);
-    if cheirality_X4 > 0
-        c4 = c4 + 1;
+for k = 1:length(Ri)
+    for i = 1:n
+        cheirality = Ri{k}(3,:) * (Xi{k}(i,:)' - Ci{k});
+        if cheirality > 0
+            nValid(k) = nValid(k) + 1;
+        end
     end
 end
-
-nValid = [c1, c2, c3, c4];
 [~,idx] = max(nValid);
-
+R = Ri{idx};
+C = Ci{idx};
+X = Xi{idx};
+nValid
 
