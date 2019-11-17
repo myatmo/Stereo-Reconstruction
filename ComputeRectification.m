e7c33de2390f626c9582d53d549fa72c1db122ff
Myat Mo <myatmo@outlook.com>
2019-11-16 21:42:02 -0600

    Update ComputeRectification.m

--- ComputeRectification.m
+++ ComputeRectification.m
@@ -6,3 +6,13 @@ function [H1, H2] = ComputeRectification(K, R, C)
 %H = KRrectRTK?1 where R is the rotation matrix of the camera. The rectified images are shown in Figure 6.
 %This rectification sends the epipoles to infinity where the epipolar line becomes horizontal.
 
+r_z_tilde = [0; 0; 1];
+r_x = C/norm(C);
+orth_proj = r_z_tilde - (dot(r_z_tilde, r_x) * r_x);
+r_z = orth_proj / norm(orth_proj);
+r_y = cross(r_z, r_x);
+R_rect = [r_x'; r_y'; r_z'];
+
+H1 = K * R_rect * inv(K);
+H2 = K * R_rect * R' * inv(K);
+
